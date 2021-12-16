fe6349c1c83b6ebfae4269f48eb220471df46a70
James Paterson <jamespaterson@Jamess-MacBook-Pro.local>
2021-12-16 03:21:24 -0500

    good start

---
+++ AA/lib.js
@@ -0,0 +1,267 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.DialUnit = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_2
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#CCCCCC").ss(1,1,1).p("AAAAVIAAgp");
+	this.shape.setTransform(0,0.025);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.DialUnit, new cjs.Rectangle(-1,-3.1,2,6.300000000000001), null);
+
+
+(lib.DialHand = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#CCCCCC").ss(1,1,1).p("AAAz/MAAAAn/");
+	this.shape.setTransform(0,-158.4);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.DialHand, new cjs.Rectangle(-1,-287.4,2,258), null);
+
+
+(lib.Crosshairs = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#CCCCCC").ss(1,1,1).p("AAAAAIAAAyAAyAAIgyAAAAAgxIAAAxIgxAA");
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.Crosshairs, new cjs.Rectangle(-6,-6,12,12), null);
+
+
+// stage content:
+(lib._lib = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.DialUnit();
+	this.instance.setTransform(540,539);
+
+	this.instance_1 = new lib.DialHand();
+	this.instance_1.setTransform(443,195.05,1,1,0,0,0,-7.5,-4.5);
+
+	this.instance_2 = new lib.Crosshairs();
+	this.instance_2.setTransform(350,350);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_2},{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(694.5,262.7,-154,279.00000000000006);
+// library properties:
+lib.properties = {
+	id: '1B1D331872B84B678B30A74AB80E74A9',
+	width: 700,
+	height: 700,
+	fps: 30,
+	color: "#333333",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['1B1D331872B84B678B30A74AB80E74A9'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
---
+++ scripts/index.js
@@ -0,0 +1,154 @@
+"use strict"
+
+/////////////////////////// 
+/////////////////////////// 
+function fxSample(arr) {
+  return arr[Math.floor(fxrand()*arr.length)]
+}
+///////////////////////////
+///////////////////////////
+
+let AALib = document.createElement("script")
+let lib
+let hammer
+let crosshairs
+let dialHand
+let dialFace
+let dialUnits
+let currentRot = 0
+let lastRot = 0
+let faceRadius = 300
+let numUnits = Math.floor(Math.random()*200) + 100
+
+AALib.setAttribute("src", "AA/lib.js")
+document.body.appendChild(AALib)
+AALib.addEventListener("load", () => {
+  let comp=AdobeAn.getComposition("1B1D331872B84B678B30A74AB80E74A9")
+  lib=comp.getLibrary()
+  kickoff()
+}, false)
+
+function kickoff() {
+  cjs.Ticker.framerate = 30
+  createjs.Ticker.addEventListener("tick", tick)
+
+
+// let swiper = new Hammer(element, {'recognizers' : [[Hammer.Pan, {direction : Hammer.DIRECTION_NONE}]]});
+
+  window.addEventListener("keydown", e => {
+    console.log("KEY: ", e)
+    if (e.key == "g") {
+    }
+  })
+  let hammer = new Hammer(document, {
+    'recognizers' : 
+      [[Hammer.Pan, {
+        direction: Hammer.DIRECTION_ALL,
+        threshold: 1
+      }]]
+  })
+
+  hammer.on('panstart', e => {
+    currentRot = getAngle(container, e.center)
+    updateRot(e)
+  })
+  hammer.on('panmove', e => {
+    updateRot(e)
+  })
+  hammer.on('panend', e => {
+    updateRot(e)
+    // console.log("panend: ", dialHand.rotation % 360)
+    // dialHand.rotation = dialHand.rotation
+    snapToClosestDialUnit()
+  })
+  
+  crosshairs = new lib.Crosshairs()
+  container.addChild(crosshairs)
+
+  dialHand = new lib.DialHand()
+  container.addChild(dialHand)
+
+  // let dialUnit = lib.DialUnit()
+
+  makeDialUnits(numUnits)
+
+}
+
+function tick(e) {
+  stage.update()
+}
+
+function getAngle(p1, p2) {
+  let angle = Math.atan2(p1.y - p2.y, p1.x - p2.x) * 180 / Math.PI
+  console.log("angle: ", angle)
+  return angle
+}
+
+function updateRot(e) {
+  lastRot = currentRot
+  currentRot = getAngle(container, e.center)
+  let diff = currentRot - lastRot
+  // console.log("diff: ", diff, dial)
+  dialHand.rotation += diff
+}
+
+function makeDialUnits(numUnits) {
+
+  dialFace = new cjs.Container()
+  dialUnits = _.times(numUnits, makeDialUnit)
+  container.addChild(dialFace)
+
+  function makeDialUnit(index) {
+    let degrees = (360 / numUnits) * index
+    let point = PTUtils.polarDegrees(faceRadius, degrees)
+    let dialUnit = new lib.DialUnit()
+    dialUnit.rotation = degrees
+    dialUnit.x = point.x
+    dialUnit.y = point.y
+    dialFace.addChild(dialUnit)
+    return dialUnit
+    // console.log('point: ', point)
+  }
+}
+
+
+function snapToClosestDialUnit() {
+  let dialRot = dialHand.rotation % 360
+  dialRot = dialRot < 0 ? (360 + dialRot) % 360 : dialRot
+  let adjustedDialRot = (numUnits / 360) * dialRot
+
+  let dialUnitsRots = _.map(dialUnits, dialUnit => {
+    return dialUnit.rotation
+  })
+
+  var closest = dialUnitsRots.reduce((prev, curr) => {
+    return (Math.abs(curr - dialRot) < Math.abs(prev - dialRot) ? curr : prev);
+  });
+
+  dialHand.rotation = closest
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
