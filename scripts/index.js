ab5a60732d0f1805e0264b622e5849988cf90292
James Paterson <jamespaterson@Jamess-MacBook-Pro.local>
2021-12-17 02:35:49 -0500

    dial working on mobile and browser

--- scripts/index.js
+++ scripts/index.js
@@ -27,14 +27,6 @@ let line
 let units = _.times(numUnits, index => {
   return index * increment
 })
-// let click = new Pizzicato.Sound({ 
-//     source: 'file',
-//     options: { 
-//       path: './sounds/click3.wav',
-//       loop: false }
-// }, function() {
-//   console.log('sound file loaded: ', );
-// })
 
 loadLib()
 function loadLib() {
@@ -60,86 +52,52 @@ function kickoff() {
   line = new cjs.Shape()
   stage.addChild(line)
 
-  // window.addEventListener("touchstart", e => {
-  //   // console.log("ts: ", e)
-  //   // console.log("ts: ", e)
-  //   let p = new cjs.Point(e.touches[0].clientX, e.touches[0].clientY)
-  //   let angle = getAngle(container, p)
-  //   granAnglePrev = getGranularAngle(angle, numUnits)
-  //   granAngleCurrent = granAnglePrev
-  //   line.graphics.clear()
-  //   line.graphics.setStrokeStyle(1).beginStroke("rgba(255,0,0,1)")
-  //   line.graphics.moveTo(container.x, container.y)
-  //   line.graphics.lineTo(p.x, p.y)
-  //   line.graphics.endStroke()
-  // })
+  document.addEventListener("touchstart", e => { handleDialStart(e.touches[0])})
+  document.addEventListener("mousedown", handleDialStart)
 
-  // window.addEventListener("touchmove", e => {
-  //   // console.log("tm: ", e.touches[0])
-  //   // console.log("co: ", container.y)
-  //   let p = new cjs.Point(e.touches[0].clientX, e.touches[0].clientY)
-  //   let angle = getAngle(container, p)
-  //   granAngleCurrent = getGranularAngle(angle, numUnits)
-  //   granAngleDelta = granAngleCurrent - granAnglePrev
-  //   if (granAngleDelta !== 0) {
-  //     click.play(0,0)
-  //     // audio.play()
-  //     dialHand.rotation += granAngleDelta
-  //   }
-  //   granAnglePrev = granAngleCurrent
-  //   line.graphics.clear()
-  //   line.graphics.setStrokeStyle(1).beginStroke("rgba(255,0,0,1)")
-  //   line.graphics.moveTo(container.x, container.y)
-  //   line.graphics.lineTo(p.x, p.y)
-  //   line.graphics.endStroke()
-  // })
+  function handleDialStart(e) {
+    let p = e.touches ? 
+      new cjs.Point(e.touches[0].clientX, e.touches[0].clientY) : 
+      new cjs.Point(e.clientX, e.clientY)
 
-
-  let hammer = new Hammer(document, {
-    'recognizers' : 
-      [[Hammer.Pan, {
-        direction: Hammer.DIRECTION_ALL,
-        threshold: 1
-      }]]
-  })
-
-  hammer.on('panstart', e => {
-    let p = new cjs.Point(e.changedPointers[0].clientX, e.changedPointers[0].clientY)
     let angle = getAngle(container, p)
-    // let angle = getAngle(container, e.center)
     granAnglePrev = getGranularAngle(angle, numUnits)
     granAngleCurrent = granAnglePrev
-    line.graphics.clear()
-    line.graphics.setStrokeStyle(1).beginStroke("rgba(255,0,0,1)")
-    line.graphics.moveTo(container.x, container.y)
-    line.graphics.lineTo(p.x, p.y)
-    line.graphics.endStroke()
+    visDialInteraction(p)
+    document.addEventListener("touchmove", handleDialMove)
+    document.addEventListener("touchend", handleDialEnd)
+    document.addEventListener("mousemove", handleDialMove)
+    document.addEventListener("mouseup", handleDialEnd)
+  }
 
-  })
-
-  hammer.on('panmove', e => {
-    console.log("asdasd: ", e)
-    let p = new cjs.Point(e.changedPointers[0].clientX, e.changedPointers[0].clientY)
+  function handleDialMove(e) {
+    let p = e.touches ? 
+      new cjs.Point(e.touches[0].clientX, e.touches[0].clientY) : 
+      new cjs.Point(e.clientX, e.clientY)
     let angle = getAngle(container, p)
-    // let angle = getAngle(container, e.center)
     granAngleCurrent = getGranularAngle(angle, numUnits)
     granAngleDelta = granAngleCurrent - granAnglePrev
     if (granAngleDelta !== 0) {
-      // click.play(0,0)
       dialHand.rotation += granAngleDelta
     }
     granAnglePrev = granAngleCurrent
+    visDialInteraction(p)
+  }
+
+  function handleDialEnd(e) {
+    document.removeEventListener("touchmove", handleDialMove)
+    document.removeEventListener("touchend", handleDialEnd)
+    document.removeEventListener("mousemove", handleDialMove)
+    document.removeEventListener("mouseup", handleDialEnd)
+  }
+
+  function visDialInteraction(p) {
     line.graphics.clear()
     line.graphics.setStrokeStyle(1).beginStroke("rgba(255,0,0,1)")
     line.graphics.moveTo(container.x, container.y)
     line.graphics.lineTo(p.x, p.y)
     line.graphics.endStroke()
-  })
-
-  hammer.on('panend', e => {
-    // updateRot(e)
-    // snapToClosestDialUnit()
-  })
+  }
   
   crosshairs = new lib.Crosshairs()
   container.addChild(crosshairs)
@@ -161,25 +119,11 @@ function getAngle(p1, p2) {
   return angle
 }
 
-function getAngleRaw(x1, y1, x2, y2) {
-  let angle = (Math.atan2(y1 - y2, x1 - x2) * 180 / Math.PI) - 90
-  angle = angle < 0 ? 360 + angle : angle
-  return angle
-}
-
 function getGranularAngle(angle, numUnits) {
   let closest = units.reduce((prev, curr) => {
     return (Math.abs(curr - angle) < Math.abs(prev - angle) ? curr : prev);
   })
   return closest
-}
-
-function updateRot(e) {
-  lastRot = currentRot
-  currentRot = getAngle(container, e.center)
-  let diff = currentRot - lastRot
-  // console.log("diff: ", diff, dial)
-  dialHand.rotation += diff
 }
 
 function makeDialUnits(numUnits) {
@@ -200,24 +144,6 @@ function makeDialUnits(numUnits) {
     // console.log('point: ', point)
   }
 }
-
-
-function snapToClosestDialUnit() {
-  let dialRot = dialHand.rotation % 360
-  dialRot = dialRot < 0 ? (360 + dialRot) % 360 : dialRot
-  let adjustedDialRot = (numUnits / 360) * dialRot
-
-  let dialUnitsRots = _.map(dialUnits, dialUnit => {
-    return dialUnit.rotation
-  })
-
-  var closest = dialUnitsRots.reduce((prev, curr) => {
-    return (Math.abs(curr - dialRot) < Math.abs(prev - dialRot) ? curr : prev);
-  });
-
-  dialHand.rotation = closest
-}
-
 
 
 
