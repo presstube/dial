54810a56341038d587c37b668429f7dac7346f47
James Paterson <jamespaterson@Jamess-MacBook-Pro.local>
2022-01-04 20:11:20 -0500

    decorated the color schemes with placeholder names

--- scripts/index.js
+++ scripts/index.js
@@ -16,6 +16,8 @@ function fxSampleRarityID(arr, rarityTarget) {
 ///////////////////////////
 ///////////////////////////
 
+const features = {}
+
 let AALoaderLib = document.createElement("script")
 let AALib = document.createElement("script")
 let AALibPile = document.createElement("script")
@@ -65,44 +67,39 @@ let units = _.times(numUnits, index => {
 
 let colorschemes = [
 
-
-  ["0e0004","31081f","6b0f1a","b91372","fa198b","b7ad99","ff4365","00d9c0","fffff3"], // chiascurro chroma squelch
-  ["590d22","800f2f","a4133c","c9184a","ff4d6d","ff758f","d2fd35","ffccd5","fff0f3"], // raspberry sorbet
-  ["d8f3dc","b7e4c7","ff0a50","74c69d","52b788","40916c","2d6a4f","1b4332","081c15"], // mint strawberry
-  ["ffd9da","ea638c","89023e","30343f","1b2021","93b7be","798071","623cea","16302b"], // vibrant gay decepticon
-  ["9a8f97","c3baba","e9e3e6","b2b2b2","736f72","a30b37","40bcd8","355070","49beaa"], // minty mud puddle
-  ["2d2a32","ddd92a","eae151","eeefa8","fafdf6","cc2936","6b818c","eee5e9","c7e8f3"], // lemonyacht
-  ["7cfef0","6bffb8","2ceaa3","28965a","2a6041","090909","4b5043","a40e4c","2c2c54"], // purple fusia minty
-  ["bbe1c3","a7cdbd","869d7a","91785d","8b5d33","433e0e","e0b0d5","ffa552","ba5624"], // cordoroy ashtray
-  ["4d5057","4e6e5d","4da167","3bc14a","cfcfcf","ed1c24","fdfffc","f1d302","f2e2d2"], // primarygreyloge
-  ["985f99","9684a1","aaacb0","b6c9bb","bfedc1","2d0320","0d160b","5c1a1b","5c573e"], // desaturated pony
-  ["104f55","32746d","9ec5ab","01200f","011502","885053","fe5f55","23022e","ffa69e"], // pastel fragrant domicile
-  ["78c0e0","449dd1","192bc2","150578","0e0e52","6eeb83","e4ff1a","ffb800","ff5714"], // horrendous pop
-  ["0d1f2d","546a7b","9ea3b0","fae1df","e4c3ad","6d2e46","ffe74c","ff5964","f9db6d"], // pastel fruit balance
-  ["007f5f","2b9348","55a630","80b918","aacc00","bfd200","eb4833","eeef20","ffff3f"], // slighty rotting apple
-  ["247ba0","70c1b3","b2dbbf","f3ffbd","ff1654","30362f","625834","da7422","8b2635"], // vibrant puce plop
-
-  // // harmonious? 
-  ["331832","d81e5b","f0544f","c6d8d3","fdf0d5"], // sky pony harmony
-  ["1b2f33","28502e","47682c","8c7051","ef3054"], // forest strawberry pop
-  ["d8e2dc","ffffff","ffcad4","f4acb7","9d8189"], // overcast barbie maxi flesh
-  ["5c415d","694966","74526c","dbd053","c89933"], // gentle lemon aubergine
-  ["fbc2b5","ffa8a9","f786aa","a14a76","cdb2ab"], // barbie's maxi flesh
-  ["ea526f","070600","f7f7ff","23b5d3","279af1"], // dashing primary robot
-  ["c4bbaf","a5978b","5c4742","8d5b4c","5a2a27"], // desert chocolate
-  ["07020d","5db7de","f1e9db","a39b8b","716a5c"], // harsh beige bot
-
-  // // also harmonious?
-  ["98eb59","801912","cf1d11","ed5d53","c2534c"], // acid lime bloodbath
-  ["1d1e18","6b8f71","aad2ba","d9fff5","b9f5d8"], // pale green dark contrast
-  ["f45b69","f6e8ea","22181c","5a0001","f13030"], // midnight rouge tron
-  ["527a2c","ebb2ce","e6177b","6b4a5a","ffc7e2"], // lovely watermelon
-  ["000000","ffffff","494949","7c7a7a","ff5d73"], // grey pink monochrome
-  ["25ced1","ffffff","fceade","ff8a5b","ea526f"], // blue orange pink pastelly
-  ["87946a","261e38","d631a2","c9bce6","381a7a"], // fairly extreme pink purple grey
-
+{scheme: ["0e0004","31081f","6b0f1a","b91372","fa198b","b7ad99","ff4365","00d9c0","fffff3"], name: "chiascurro chroma squelch"},
+{scheme: ["590d22","800f2f","a4133c","c9184a","ff4d6d","ff758f","d2fd35","ffccd5","fff0f3"], name: "raspberry sorbet"},
+{scheme: ["d8f3dc","b7e4c7","ff0a50","74c69d","52b788","40916c","2d6a4f","1b4332","081c15"], name: "mint strawberry"},
+{scheme: ["ffd9da","ea638c","89023e","30343f","1b2021","93b7be","798071","623cea","16302b"], name: "vibrant prancy decepticon"},
+{scheme: ["9a8f97","c3baba","e9e3e6","b2b2b2","736f72","a30b37","40bcd8","355070","49beaa"], name: "minty mud puddle"},
+{scheme: ["2d2a32","ddd92a","eae151","eeefa8","fafdf6","cc2936","6b818c","eee5e9","c7e8f3"], name: "lemonyacht"},
+{scheme: ["7cfef0","6bffb8","2ceaa3","28965a","2a6041","090909","4b5043","a40e4c","2c2c54"], name: "purple fusia minty"},
+{scheme: ["bbe1c3","a7cdbd","869d7a","91785d","8b5d33","433e0e","e0b0d5","ffa552","ba5624"], name: "cordoroy ashtray"},
+{scheme: ["4d5057","4e6e5d","4da167","3bc14a","cfcfcf","ed1c24","fdfffc","f1d302","f2e2d2"], name: "primarygreyloge"},
+{scheme: ["985f99","9684a1","aaacb0","b6c9bb","bfedc1","2d0320","0d160b","5c1a1b","5c573e"], name: "desaturated pony"},
+{scheme: ["104f55","32746d","9ec5ab","01200f","011502","885053","fe5f55","23022e","ffa69e"], name: "pastel fragrant domicile"},
+{scheme: ["78c0e0","449dd1","192bc2","150578","0e0e52","6eeb83","e4ff1a","ffb800","ff5714"], name: "horrendous pop"},
+{scheme: ["0d1f2d","546a7b","9ea3b0","fae1df","e4c3ad","6d2e46","ffe74c","ff5964","f9db6d"], name: "pastel fruit balance"},
+{scheme: ["007f5f","2b9348","55a630","80b918","aacc00","bfd200","eb4833","eeef20","ffff3f"], name: "slighty rotting apple"},
+{scheme: ["247ba0","70c1b3","b2dbbf","f3ffbd","ff1654","30362f","625834","da7422","8b2635"], name: "vibrant puce plop"},
+{scheme: ["331832","d81e5b","f0544f","c6d8d3","fdf0d5"], name: "sky pony harmony"},
+{scheme: ["1b2f33","28502e","47682c","8c7051","ef3054"], name: "forest strawberry pop"},
+{scheme: ["d8e2dc","ffffff","ffcad4","f4acb7","9d8189"], name: "overcast barbie maxi flesh"},
+{scheme: ["5c415d","694966","74526c","dbd053","c89933"], name: "gentle lemon aubergine"},
+{scheme: ["fbc2b5","ffa8a9","f786aa","a14a76","cdb2ab"], name: "barbie's maxi flesh"},
+{scheme: ["ea526f","070600","f7f7ff","23b5d3","279af1"], name: "dashing primary robot"},
+{scheme: ["c4bbaf","a5978b","5c4742","8d5b4c","5a2a27"], name: "desert chocolate"},
+{scheme: ["07020d","5db7de","f1e9db","a39b8b","716a5c"], name: "harsh beige bot"},
+{scheme: ["98eb59","801912","cf1d11","ed5d53","c2534c"], name: "acid lime bloodbath"},
+{scheme: ["1d1e18","6b8f71","aad2ba","d9fff5","b9f5d8"], name: "pale green dark contrast"},
+{scheme: ["f45b69","f6e8ea","22181c","5a0001","f13030"], name: "midnight rouge tron"},
+{scheme: ["527a2c","ebb2ce","e6177b","6b4a5a","ffc7e2"], name: "lovely watermelon"},
+{scheme: ["000000","ffffff","494949","7c7a7a","ff5d73"], name: "grey pink monochrome"},
+{scheme: ["25ced1","ffffff","fceade","ff8a5b","ea526f"], name: "blue orange pink pastelly"},
+{scheme: ["87946a","261e38","d631a2","c9bce6","381a7a"], name: "fairly extreme pink purple grey"},
 
 ]
+
 
 
 loadLoaderLib()
@@ -409,9 +406,9 @@ function loadIteration(iter) {
     return objkt.iteration == iteration;
   }), 'generationHash');
   bootFXHash(fxhash)
-  // rarityTarget = fxrand()
-  rarityTarget = 1
-  currentColorScheme = fxSampleRarity(colorschemes, rarityTarget)
+  rarityTarget = fxrand()
+  // rarityTarget = 1
+  currentColorScheme = fxSampleRarity(colorschemes, rarityTarget).scheme
   // currentColorScheme = fxSample(colorschemes)
   currentBGColor = fxSample(currentColorScheme)
   colorBG("#" + currentBGColor)
@@ -618,9 +615,9 @@ function makeDialUnits(objkts) {
 
     // seems dangerous to have this in here if I'm sorting...
     bootFXHash(objktData.generationHash)
-    // rarityTarget = fxrand()
-    rarityTarget = 1
-    currentColorScheme = fxSampleRarity(colorschemes, rarityTarget)
+    rarityTarget = fxrand()
+    // rarityTarget = 1
+    currentColorScheme = fxSampleRarity(colorschemes, rarityTarget).scheme
     // currentColorScheme = fxSample(colorschemes)
     currentBGColor = fxSample(currentColorScheme)
 
@@ -855,10 +852,10 @@ function startPrincess(iteration) {
   }), 'generationHash');
   bootFXHash(fxhash)
   // setting up color scheme, choosing bg color, painting bg
-  // rarityTarget = fxrand()
-  rarityTarget = 1
+  rarityTarget = fxrand()
+  // rarityTarget = 1
   console.log("rarityTarget: ", rarityTarget)
-  currentColorScheme = fxSampleRarity(colorschemes, rarityTarget)
+  currentColorScheme = fxSampleRarity(colorschemes, rarityTarget).scheme
   // currentColorScheme = fxSample(colorschemes)
   currentBGColor = fxSample(currentColorScheme)
   colorBG("#" + currentBGColor)
